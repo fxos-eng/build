e565b91fadd34e78b242a11dc88665fd597c8b2c
Kevin Grandon <kevingrandon@yahoo.com>
2014-12-13 12:50:40 -0800

    Run linters on js dir

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var eslint = require('gulp-eslint');
 gulp.task('lint', function () {
 	// Note: To have the process exit with an error code (1) on
 	//  lint error, return the stream and pipe to failOnError last.
-	return gulp.src(['*.js'])
+	return gulp.src(['./../../app/**.js'])
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failOnError());
